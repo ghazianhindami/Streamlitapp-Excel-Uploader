3d794ab492fdea479d274720c664bbe21d773899
Rintaldi Ghazian Hindami <ghazianhindami35@gmail.com>
2026-03-10 21:45:53 +0700

    ganti port ke 02

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import subprocess
 
 def main():
     script_path = os.path.abspath("Apps/app.py")
-    url = "http://localhost:8501"
+    url = "http://localhost:8502"
 
     # buka browser dulu
     webbrowser.open(url)
@@ -14,7 +14,7 @@ def main():
     subprocess.run([
         "streamlit", "run", script_path,
         "--server.headless=true",
-        "--server.port=8501",
+        "--server.port=8502",
         "--browser.serverAddress=localhost",
         "--server.fileWatcherType=none"
     ])
